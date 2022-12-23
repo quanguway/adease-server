debb85126f8eb2d3d047ac24b95ffd8a9adc32e4
quanguway <quangnguyen.uway@gmail.com>
2022-12-23 11:50:48 +0700

    change host

--- config/server.ts
+++ config/server.ts
@@ -1,5 +1,5 @@
 export default ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
+  host: env("HOST", "https://adease-server-lf74kthi5-quanguway.vercel.app"),
   port: env.int("PORT", 1337),
   app: {
     keys: env.array("APP_KEYS"),
