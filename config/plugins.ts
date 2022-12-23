225e357b4b3a6fc6c30f2fb9ebb10ca8ed5c0a76
quanguway <quangnguyen.uway@gmail.com>
2022-12-23 11:48:13 +0700

    changefix host

--- config/plugins.ts
+++ config/plugins.ts
@@ -4,6 +4,8 @@ export default ({ env }) => ({
 			deployHook:
 				"https://api.vercel.com/v1/integrations/deploy/prj_003db1StIF3F7OCnV6Wj7tzj5jaK/HTmFv7e98C",
 			apiToken: "eyyhoFE9BUI9gw2jy5DpchEy",
+			appFilter: "adease-server",
+			roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
 		},
 		enabled: true,
 	},
