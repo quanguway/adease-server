c2b55027b7f69c564ac16e9d78d7dd4ac52d8656
quanguway <quangnguyen.uway@gmail.com>
2022-12-23 10:30:44 +0700

    deploy

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,10 +1,10 @@
 export default ({ env }) => ({
 	"vercel-deploy": {
+		config: {
+			deployHook:
+				"https://api.vercel.com/v1/integrations/deploy/prj_003db1StIF3F7OCnV6Wj7tzj5jaK/HTmFv7e98C",
+			apiToken: "eyyhoFE9BUI9gw2jy5DpchEy",
+		},
 		enabled: true,
-	},
-	config: {
-		deployHook:
-			"https://api.vercel.com/v1/integrations/deploy/prj_003db1StIF3F7OCnV6Wj7tzj5jaK/HTmFv7e98C",
-		apiToken: "eyyhoFE9BUI9gw2jy5DpchEy",
 	},
 });
