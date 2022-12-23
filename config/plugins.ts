29c9a1af7e29ead643982ce05f42e9a2110172fa
quanguway <quangnguyen.uway@gmail.com>
2022-12-23 10:24:29 +0700

    deploy

--- config/plugins.ts
+++ config/plugins.ts
@@ -6,7 +6,5 @@ export default ({ env }) => ({
 		deployHook:
 			"https://api.vercel.com/v1/integrations/deploy/prj_003db1StIF3F7OCnV6Wj7tzj5jaK/HTmFv7e98C",
 		apiToken: "eyyhoFE9BUI9gw2jy5DpchEy",
-		appFilter: "adease-server",
-		roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
 	},
 });
